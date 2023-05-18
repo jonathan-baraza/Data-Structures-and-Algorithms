dab60e6bfcd0792b90d0183347556173e3b7bc2b
jonathan_baraza <jbaraza46@gmail.com>
2023-05-18 10:36:23 +0300

    linear search functions

--- linear_search.py
+++ linear_search.py
@@ -2,13 +2,21 @@ def linear_search(list,target):
     """
     Returns the index position of the target it found, else returns None
     """
+    #answer one
     # print(list.index(target))
-    x=0;
-    while(x<len(list)):
-        if(list[x]==target):
-            break;
-        x=x+1
-    print(x)
 
+    #answer two
+    # x=0;
+    # while(x<len(list)):
+    #     if(list[x]==target):
+    #         break;
+    #     x=x+1
+    # print(x)
 
-linear_search([2,3,4,5],2)
+    #answer three
+    for i in range(0,len(list)):
+        if list[i]==target:
+            return i
+    return None
+
+print(linear_search([2,3,4,5],3))
