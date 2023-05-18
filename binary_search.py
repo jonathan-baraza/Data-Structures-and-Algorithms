b074b48170d878921b7b5df5176b149397239e52
jonathan_baraza <jbaraza46@gmail.com>
2023-05-18 11:47:50 +0300

    code cleanup

--- binary_search.py
+++ binary_search.py
@@ -1,38 +1,20 @@
 import math
-
-
 def binary_search(list,target):
     myList=list
     myList.sort()
-
     midValue=None;
     newList=myList
 
     while len(newList)>0:
         midValue=newList[math.floor(len(newList) / 2)]
-        print("midValue up")
-        print(midValue)
         if midValue==target:
-            print("hitting this one")
             return myList.index(midValue)
         else:
             if midValue>target:
                 newList = newList[0:newList.index(midValue)]
-
             else:
-                print("mid value less than")
                 newList= newList[newList.index(midValue)+1:]
-
-
-
-
-
-        print("newList")
-        print(newList)
-
     return None
-
-
 
 
 def verifyResult(result):
@@ -43,9 +25,7 @@ def verifyResult(result):
 
 
 
-
 numbers=[1,2,3,4,5,6,7,8,9,10]
-
 verifyResult(binary_search(numbers,10))
 
 
