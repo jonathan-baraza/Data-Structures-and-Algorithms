ba2afa69e24c8f13ba4754905ec688e273fe72b1
jonathan_baraza <jbaraza46@gmail.com>
2023-05-22 13:31:34 +0300

    recursive binary search

--- DS/arrays.py
+++ DS/arrays.py
@@ -1,2 +1,14 @@
+
 new_list=[1,2,3]
-print(type(new_list))
+result=new_list[0]
+
+#Searching
+if 1 in new_list: print(True)
+
+#or
+for x in new_list:
+    if x==1:
+        print(new_list.index(x))
+        break
+
+print(result)
