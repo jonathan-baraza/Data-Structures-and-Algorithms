c320246fe73646931be14ccb507648f7e7ef173f
jonathan_baraza <jbaraza46@gmail.com>
2023-05-29 11:11:55 +0300

    merge sort on linked list function documentation

--- linked_list_merge_sort.py
+++ linked_list_merge_sort.py
@@ -1,3 +1,11 @@
 from DS.linked_list import LinkedList,Node
 
+def merge_sort(linked_list):
+    """
+    This function sorts a linked list in ascending order
+    -Recursively divide the linked list into sublists containing a single node
+    -Repeatedly merge the sublist to produce sorted sublists until one remains
 
+    :param linked_list:
+    :return: a sorted linked list
+    """
