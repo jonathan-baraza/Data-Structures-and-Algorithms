0b098e8746cfd9022a493601926e200bb946328c
jonathan_baraza <jbaraza46@gmail.com>
2023-05-29 11:13:25 +0300

    splif function on linked list documentation

--- linked_list_merge_sort.py
+++ linked_list_merge_sort.py
@@ -9,3 +9,22 @@ def merge_sort(linked_list):
     :param linked_list:
     :return: a sorted linked list
     """
+
+    if linked_list.size()==1:
+        return linked_list
+    elif linked_list.head is None:
+        return linked_list
+
+    left_half,right_half=split(linked_list)
+    left=merge_sort(left_half)
+    right=merge_sort(right_half)
+
+    return merge(left,right)
+
+
+def split (linked_list):
+    """
+    divide the unsorted list at midpoint into sublinkedlist
+    :param linked_list:
+    :return:
+    """
