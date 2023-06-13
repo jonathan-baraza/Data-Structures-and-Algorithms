6c4d152d4f308ee675bb1a07602e25b28c81219f
jonathan_baraza <jbaraza46@gmail.com>
2023-06-13 21:06:57 +0300

    fn to add element to a set

--- js/set.js
+++ js/set.js
@@ -2,7 +2,7 @@
 
 function mySet() {
   //the var collection will hold the set
-  var collection = ["1", 2, "dog", "cat"];
+  var collection = [1, 2, 3, 4, 5];
 
   //this method will check for the presence of an element and return true or false
   this.has = function (element) {
@@ -13,9 +13,17 @@ function mySet() {
   this.values = function () {
     return collection;
   };
+
+  //this method will add an element to the set
+  this.add = function (element) {
+    if (!this.has(element)) {
+      collection.push(element);
+      return true;
+    }
+    return false;
+  };
 }
 
 var obj = new mySet();
-console.log(obj.has("1"));
-console.log(obj.has("bike"));
+console.log(obj.add(1));
 console.log(obj.values());
