4d1187d5e0539643d736ca33300f22860935b7e6
jonathan_baraza <jbaraza46@gmail.com>
2023-06-13 20:57:02 +0300

    fn to check if set contains a value

--- js/set.js
+++ js/set.js
@@ -1,1 +1,11 @@
 /*Sets*/
+
+function mySet() {
+  //the var collection will hold the set
+  var collection = [];
+
+  //this method will check for the presence of an element and return true or false
+  this.has = function (element) {
+    return collection.indexOf(element) !== -1;
+  };
+}
