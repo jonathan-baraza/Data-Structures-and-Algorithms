612931b30ffad282ce2de51c8f959769b687efeb
jonathan_baraza <jbaraza46@gmail.com>
2023-06-13 21:03:23 +0300

    fn to return all set values

--- js/set.js
+++ js/set.js
@@ -2,10 +2,20 @@
 
 function mySet() {
   //the var collection will hold the set
-  var collection = [];
+  var collection = ["1", 2, "dog", "cat"];
 
   //this method will check for the presence of an element and return true or false
   this.has = function (element) {
     return collection.indexOf(element) !== -1;
   };
+
+  //this method will return all values in a set
+  this.values = function () {
+    return collection;
+  };
 }
+
+var obj = new mySet();
+console.log(obj.has("1"));
+console.log(obj.has("bike"));
+console.log(obj.values());
