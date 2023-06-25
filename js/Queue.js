1acfe934f8d42e05948220c4f82865947c781070
jonathan_baraza <jbaraza46@gmail.com>
2023-06-25 10:11:44 +0300

    fn to add element to the queue

--- js/Queue.js
+++ js/Queue.js
@@ -1,6 +1,25 @@
 function Queue() {
-  collection = [];
+  var collection = [];
   const print = () => {
     console.log(collection);
   };
+
+  const enqueue = (element) => {
+    collection.push(element);
+  };
+
+  //   const dequeue = () => {
+  //     return collection.shift();
+  //   };
+  //   const front = () => {
+  //     return collection[0];
+  //   };
+
+  //   const size = () => {
+  //     return collection.length;
+  //   };
+
+  //   const isEmpty = () => {
+  //     return collection.length === 0;
+  //   };
 }
